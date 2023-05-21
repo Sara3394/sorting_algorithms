e22129dd9dd153f75288d18a32d6f5a775cb8fb9
Sara3394 <hailu.serafina@yahoo.com>
2023-05-21 07:05:25 +0300

    fix betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,6 +3,7 @@
 * selection_sort - sorts an array of integers in ascending order
 *@array: array to be sorted
 *@size: size of the array
+*Return: void
 */
 
 void selection_sort(int *array, size_t size)
