7c8386bfd00709bfe80867c3f1b6bf629c794775
Sara3394 <hailu.serafina@yahoo.com>
2023-05-21 06:52:26 +0300

    task 2 update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,8 +7,7 @@
 
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_index;
-int temp;
+size_t i, j, min_index, temp;
 if (array == NULL || size < 2)
 return;
 for (i = 0; i < size - 1; i++)
@@ -19,12 +18,13 @@ for (j = i + 1; j < size; j++)
 if (array[j] < array[min_index])
 min_index = j;
 }
-if (min_index != i)
+if (array[min_index] != array[i])
 {
 temp = array[i];
 array[i] = array[min_index];
 array[min_index] = temp;
-}
+
 print_array(array, size);
+}
 }
 }
